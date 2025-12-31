631d404e936675d6225dfa37b0e825a57bf257e4
Kai-Yu She <kaiyushe@gmail.com>
2025-12-30 17:13:58 -0800

    Current state of pi.c before rewrite

--- pi/pi.c
+++ pi/pi.c
@@ -85,13 +85,16 @@ void chudnovsky(mpf_t r_pi, long n, int threads, unsigned long prec_bits)
     // to retain accuracy, denominator is initially an integer
     mpf_set_default_prec(prec_bits);
 
-    mpz_t P[threads];
-    mpz_t Q[threads];
-    mpz_t R[threads];
-    for (int i = 0; i < threads; i++)
-    {
-        mpz_inits(P[i], Q[i], R[i], NULL);
-    }
+    // mpz_t P[threads];
+    // mpz_t Q[threads];
+    // mpz_t R[threads];
+    // for (int i = 0; i < threads; i++)
+    // {
+    //     mpz_inits(P[i], Q[i], R[i], NULL);
+    // }
+
+    mpz_t P, Q, R;
+    mpz_inits(P, Q, R, NULL);
 
     mpz_t int_den;
     mpz_init(int_den);
@@ -100,93 +103,84 @@ void chudnovsky(mpf_t r_pi, long n, int threads, unsigned long prec_bits)
     
     // multiprocessing implementation
     long interval = n / threads;
-    printf("n %ld\nthreads %d\n", n, threads);
-    printf("interval %ld\n", interval);
+    fprintf(stderr, "n %ld\nthreads %d\n", n, threads);
+    fprintf(stderr, "interval %ld\n", interval);
     
-    #pragma omp parallel for num_threads(threads)
+    #pragma omp parallel for num_threads(threads) ordered
     for (int i = 0; i < threads - 1; i++)
     {
         long start = i * interval + 1;
         long end = (i + 1) * interval + 1;
-        // printf("%ld %ld\n", start, end);
-        binary_split(P[i], Q[i], R[i], start, end);
+        mpz_t Pab, Qab, Rab;
+        mpz_inits(Pab, Qab, Rab, NULL);
+        binary_split(Pab, Qab, Rab, start, end);
+
+        #pragma omp ordered
+        {
+            // if first iteration, set P, Q, R to results
+            if (i == 0)
+            {
+                mpz_set(P, Pab);
+                mpz_set(Q, Qab);
+                mpz_set(R, Rab);
+            }
+            else 
+            {
+                mpz_t Pab_Rmb;
+                mpz_init(Pab_Rmb);
+                mpz_mul(Pab_Rmb, P, Rab);
+                mpz_mul(R, Qab, R);
+                mpz_add(R, R, Pab_Rmb);
+                
+                mpz_mul(P, P, Pab);
+                mpz_mul(Q, Q, Qab);
+                mpz_clear(Pab_Rmb);
+            }
+        }
+        mpz_clears(Pab, Qab, Rab, NULL);
     }
-    // printf("%ld %ld\n", (threads - 1) * interval + 1, n);
-    binary_split(P[threads - 1], Q[threads - 1], R[threads - 1], (threads - 1) * interval + 1, n);
+    mpz_t Pab, Qab, Rab;
+    mpz_inits(Pab, Qab, Rab, NULL);
+    binary_split(Pab, Qab, Rab, (threads - 1) * interval + 1, n);
+    mpz_t Pab_Rmb;
+    mpz_init(Pab_Rmb);
+    mpz_mul(Pab_Rmb, P, Rab);
+    mpz_mul(R, Qab, R);
+    mpz_add(R, R, Pab_Rmb);
+    mpz_mul(P, P, Pab);
+    mpz_mul(Q, Q, Qab);
+    mpz_clear(Pab_Rmb);
+    mpz_clears(Pab, Qab, Rab, NULL);
     #pragma omp barrier
     
-    // multithreaded combination
-    int curr_length = threads; // current length of the result arrays
-    while (curr_length > 1)
-    {
-        // combine results in pairs. This can be done in parallel
-        #pragma omp parallel for num_threads(curr_length/2)
-        for (int i = 0; i < curr_length; i += 2)
-        {
-            // calculate R first before P and Q are modified
-            // eliminates the need for temp variables -> less memory used
-            mpz_t P_R; // for calculating second term of R
-            mpz_init(P_R);
-            mpz_mul(P_R, P[i], R[i+1]);
-            mpz_mul(R[i], Q[i+1], R[i]);
-            mpz_add(R[i], R[i], P_R);
-            mpz_clear(P_R);
-            mpz_mul(P[i], P[i], P[i+1]);
-            mpz_mul(Q[i], Q[i], Q[i+1]);
-        }
-        #pragma omp barrier
-        // move calculated values to first half of the array
-        // also move last value if curr_length is odd
-        for (int i = 0; i < (curr_length / 2); i++)
-        {
-            mpz_set(P[i], P[i*2]);
-            mpz_set(Q[i], Q[i*2]);
-            mpz_set(R[i], R[i*2]);
-        }
-        if (curr_length % 2 == 1)
-        {
-            mpz_set(P[curr_length/2], P[curr_length-1]);
-            mpz_set(Q[curr_length/2], Q[curr_length-1]);
-            mpz_set(R[curr_length/2], R[curr_length-1]);
-        }
-        // clear previous second half
-        // the initial i value is the next curr_length value
-        for (int i = (int)((double)curr_length / 2 + 0.5); i < curr_length; i++)
-        {
-            mpz_clears(P[i], Q[i], R[i], NULL);
-        }
-        // set curr_length to half of previous length, rounded to account for odd length
-        curr_length = (int)((double)curr_length / 2 + 0.5);
-    }
-
-    mpf_set_z(f_Qab, Q[0]);
+    mpf_set_z(f_Qab, Q);
     
-    printf("calculating numerator\n");
+    fprintf(stderr, "calculating numerator\n");
     mpf_set_d(r_pi, 10005.0);
     mpf_sqrt(r_pi, r_pi);
     mpf_mul_ui(r_pi, r_pi, 426880);
     mpf_mul(r_pi, r_pi, f_Qab);
     
-    printf("calculating denominator\n");
-    mpz_mul_ui(int_den, Q[0], 13591409);
-    mpz_add(int_den, int_den, R[0]);
+    fprintf(stderr, "calculating denominator\n");
+    mpz_mul_ui(int_den, Q, 13591409);
+    mpz_add(int_den, int_den, R);
     mpf_set_z(den, int_den);
+    mpz_clear(int_den);
     
     mpf_div(r_pi, r_pi, den);
 
-    mpz_clears(P[0], Q[0], R[0], NULL);
+    mpz_clears(P, Q, R, NULL);
     mpf_clears(den, f_Qab, NULL);
     return;
 }
 
 int main(int argc, char* argv[])
 {
-    printf("getting prec\n");
-    unsigned long prec = atol(argv[1]);
-    // unsigned long prec = 200;
+    // unsigned long prec = atol(argv[1]);
+    unsigned long prec = 20;
     unsigned long prec_bits = (prec + 2) * BITS_PER_DIGIT + 3;
-    printf("prec: %ld\n", prec);
-    printf("prec_bits: %ld\n", prec_bits);
+    fprintf(stderr, "precision: %ld\n", prec);
+    fprintf(stderr, "prec_bits: %ld\n", prec_bits);
     mpf_set_default_prec(prec_bits);
     mpf_t pi;
     mpf_init(pi);
